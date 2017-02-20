63e2ecb48ae0697afb8244ec54fbd0e4b977eb21
Adam Ullman <adam@tokbox.com>
2017-02-21 10:48:34 +1100

    Update firefox path properly

--- demo/travis-start-webdriver.sh
+++ demo/travis-start-webdriver.sh
@@ -5,9 +5,10 @@
 echo "BROWSERBIN is $BROWSERBIN"
 if [[ $BROWSERBIN == *"firefox"* ]]; then
   echo "Updating path to Firefox"
-  # Update the path to firefox
-  ln -s $BROWSERBIN ./browsers/bin/firefox
-  export PATH=./browsers/bin:$PATH
+  # Update the path to firefox to take off the -$BVER
+  ln -s $BROWSERBIN ${BROWSERBIN:0:${#BROWSERBIN} - ${#BVER} - 1}
+  FIREFOX=firefox
+  export PATH=${BROWSERBIN:0:${#BROWSERBIN} - ${#BVER} - ${#FIREFOX} - 1}:$PATH
 fi
 echo "Firefox is at `which firefox`"
 webdriver-manager update
