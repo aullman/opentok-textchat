695d66a938a3ef02f0d55f43ffe604d924a4da39
Adam Ullman <adam@tokbox.com>
2017-02-21 10:52:57 +1100

    Run protractor from the start scripts

--- demo/travis-start-webdriver.sh
+++ demo/travis-start-webdriver.sh
@@ -6,8 +6,11 @@ echo "BROWSERBIN is $BROWSERBIN"
 if [[ $BROWSERBIN == *"firefox"* ]]; then
   echo "Updating path to Firefox"
   # Update the path to firefox to take off the -$BVER
+  # set ./browsers/bin/firefox-stable to ./browsers/bin/firefox
   ln -s $BROWSERBIN ${BROWSERBIN:0:${#BROWSERBIN} - ${#BVER} - 1}
   FIREFOX=firefox
+  # Set path to include ./browsers/bin so that `which firefox` picks up this path
+  # and WebDriver uses it
   export PATH=${BROWSERBIN:0:${#BROWSERBIN} - ${#BVER} - ${#FIREFOX} - 1}:$PATH
 fi
 echo "Firefox is at `which firefox`"
@@ -16,3 +19,5 @@ webdriver-manager update
 webdriver-manager start 2>/dev/null &
 # Wait 3 seconds for port 4444 to be listening connections
 while ! nc -z 127.0.0.1 4444; do sleep 3; done
+
+./node_modules/.bin/protractor tests/protractor.conf.js
