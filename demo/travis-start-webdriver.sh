c84a2ed2ef977725e414c719c1c01b20e6131815
Adam Ullman <adam@tokbox.com>
2017-02-23 22:15:31 +1100

    Giving up on Firefox for now.
    It at least works when running locally. For Travis I still don't know how to set the firefox binary until http://stackoverflow.com/questions/42356835/how-do-i-specify-the-path-to-the-firefox-binary-using-protractor-and-marionette/42358064#42358064 gets answered

--- demo/travis-start-webdriver.sh
+++ demo/travis-start-webdriver.sh
@@ -2,19 +2,8 @@
 
 # taken from https://stackoverflow.com/questions/36066695/running-protractor-when-webdriver-manager-start-finishes/39252628#39252628
 
-echo "BROWSERBIN is $BROWSERBIN"
-if [[ $BROWSERBIN == *"firefox"* ]]; then
-  echo "Updating path to Firefox"
-  # Update the path to firefox to make Webdriver use it
-  FIREFOX_PATH=`which firefox`
-  sudo mv $FIREFOX_PATH $FIREFOX_PATH-bak
-  sudo ln -s $BROWSERBIN $FIREFOX_PATH
-fi
-echo "Firefox is at `which firefox`"
 webdriver-manager update
 # Start selenium server and trash the verbose error messages from webdriver
 webdriver-manager start 2>/dev/null &
 # Wait 3 seconds for port 4444 to be listening connections
 while ! nc -z 127.0.0.1 4444; do sleep 3; done
-
-./node_modules/.bin/protractor tests/protractor.conf.js
