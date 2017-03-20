77f29b65b39c5dae5c66e1103a8f5687e9265429
Adam Ullman <adam@tokbox.com>
2017-03-20 12:20:32 +1100

    Try getting sauce labs working

--- demo/tests/protractor.conf.js
+++ demo/tests/protractor.conf.js
@@ -1,38 +1,37 @@
 const firefox = require('selenium-webdriver/firefox'); // eslint-disable-line import/no-extraneous-dependencies
 
-const firefoxProfile = new firefox.Profile();
-
 const config = {
   specs: ['e2e/scenarios.js'],
-  seleniumAddress: 'http://localhost:4444/wd/hub',
   framework: 'jasmine',
+  multiCapabilities: [{
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['auto-select-desktop-capture-source="Entire screen"',
+        'use-fake-device-for-media-stream',
+        'use-fake-ui-for-media-stream', 'disable-popup-blocking'],
+    },
+    maxInstances: 1,
+  }],
 };
 
-switch (process.env.BROWSER) {
-  case 'firefox':
-    firefoxProfile.setPreference('media.navigator.permission.disabled', true);
-    firefoxProfile.setPreference('media.navigator.streams.fake', true);
-    firefoxProfile.setPreference('media.getusermedia.screensharing.allowed_domains',
-      'localhost,adam.local');
+if (process.env.SAUCE_USERNAME && process.env.SAUCE_ACCESS_KEY) {
+  const firefoxProfile = new firefox.Profile();
+  firefoxProfile.setPreference('media.navigator.permission.disabled', true);
+  firefoxProfile.setPreference('media.navigator.streams.fake', true);
+  firefoxProfile.setPreference('media.getusermedia.screensharing.allowed_domains',
+    'localhost,adam.local');
 
-    config.capabilities = {
-      browserName: 'firefox',
-      marionette: true,
-      firefox_profile: firefoxProfile,
-    };
-    break;
-  case 'chrome':
-  default:
-    config.capabilities = {
-      browserName: 'chrome',
-      chromeOptions: {
-        args: ['auto-select-desktop-capture-source="Entire screen"',
-          'use-fake-device-for-media-stream',
-          'use-fake-ui-for-media-stream', 'disable-popup-blocking'],
-        binary: process.env.BROWSERBIN,
-      },
-    };
-    break;
+  config.multiCapabilities.push({
+    browserName: 'firefox',
+    marionette: true,
+    firefox_profile: firefoxProfile,
+    maxInstances: 1,
+  });
+
+  config.sauceUser = process.env.SAUCE_USERNAME;
+  config.sauceKey = process.env.SAUCE_ACCESS_KEY;
+} else {
+  config.seleniumAddress = 'http://localhost:4444/wd/hub';
 }
 
 exports.config = config;
